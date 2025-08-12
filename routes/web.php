d93357ef6c6311d8bd664198b0c5489c0977097a
Limitlessz7 <Andijuliansyah27@gmail.com>
2025-08-12 08:30:49 +0700

    add routes

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+   
 });
+ route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'dashboard_page'])->name('dashboard');
